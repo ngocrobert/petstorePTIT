ffb90474fb194addfffe140210452b678fee51ed
ngocrobert <nguyenxuanngoc18012001@gmail.com>
2023-10-27 14:52:45 +0700

    demo modul dang ky, dang nhap

---
+++ petstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace petstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
